d3e2efff3a80cdb2a09b78b0a88ad93a3d231d4b
shota sakumoto <s.shota@wcoki.net>
2023-06-17 07:03:45 +0900

    API更新

--- amplify/backend/function/todoFunction/src/index.py
+++ amplify/backend/function/todoFunction/src/index.py
@@ -5,11 +5,8 @@ def handler(event, context):
   print(event)
   
   return {
+      'isBase64Encoded': False,
       'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': json.dumps('Hello from your new Amplify Python lambda!')
+      'headers': {},
+      'body': '{"message": "Hello from your new Amplify Python lambda!"}'
   }
