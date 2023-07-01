617baea8bde6a4cc33368b8e6e14c7d4370b7e5f
shota sakumoto <s.shota@wcoki.net>
2023-07-01 19:16:47 +0900

    numpyインストール

--- amplify/backend/function/syouhinkanriyuia6f4c3b5/src/index.py
+++ amplify/backend/function/syouhinkanriyuia6f4c3b5/src/index.py
@@ -1,6 +1,7 @@
 import json
 import boto3
 import uuid
+from boto3.dynamodb.conditions import Key
 from decimal import Decimal
 import pandas as pd
 from io import BytesIO
@@ -51,35 +52,71 @@ def handler(event, context):
                 'id': body['id']
             })
 
-        # Excel upload アクセスの場合はこちら
-        elif body['action'] == 'excel_upload':
-            # Assume the Excel file is sent as a base64 encoded string
-            base64_excel = body['file']
-            excel_data = base64.b64decode(base64_excel)
-            excel_file = BytesIO(excel_data)
+    elif body['action'] == 'excel_upload':
+        base64_excel = body['file']
+        excel_data = base64.b64decode(base64_excel)
+        excel_file = BytesIO(excel_data)
+        df = pd.read_excel(excel_file, engine='openpyxl')
 
-            # Read the Excel file with pandas
-            df = pd.read_excel(excel_file, engine='openpyxl')
+        # Convert the dataframe to a dictionary
+        records = df.to_dict('records')
 
-            # Convert the dataframe to a JSON object
-            json_data = df.to_json(orient='records')
+        # Initialize insert and update counts
+        insert_count = 0
+        update_count = 0
 
-            # Return the JSON data
-            return {
-                'statusCode': 200,
-                'headers': {
-                    'Access-Control-Allow-Headers': '*',
-                    'Access-Control-Allow-Origin': '*',
-                    'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-                },
-                'body': json_data
+        for record in records:
+            # Add uuid as the id field if not already present
+            if 'id' not in record:
+                record['id'] = str(uuid.uuid4())
+
+            # Change E_Date to ymd format
+            record['E_Date'] = pd.to_datetime(record['E_Date'], unit='ms').strftime('%Y-%m-%d')
+
+            # Check if the record already exists
+            existing_item = app_table.query(
+                KeyConditionExpression=Key('B_OrderNumber').eq(record['B_OrderNumber']) & Key('E_Date').eq(record['E_Date'])
+            )
+
+            if existing_item['Count'] > 0:
+                # The record exists, update it
+                app_table.update_item(
+                    Key={
+                        'id': existing_item['Items'][0]['id']
+                    },
+                    UpdateExpression="set B_OrderNumber=:on, E_Date=:ed, G_SupplierCode=:sc, H_supplier=:hs, L_ItemCode=:ic, M_ItemName=:in, R_qaunty=:rq, T_unit=:tu, AB_detail1=:d1, AC_detail2=:d2",
+                    ExpressionAttributeValues={
+                        ':on': record['B_OrderNumber'],
+                        ':ed': record['E_Date'],
+                        ':sc': record['G_SupplierCode'],
+                        ':hs': record['H_supplier'],
+                        ':ic': record['L_ItemCode'],
+                        ':in': record['M_ItemName'],
+                        ':rq': record['R_qaunty'],
+                        ':tu': record['T_unit'],
+                        ':d1': record['AB_detail1'],
+                        ':d2': record['AC_detail2']
+                    }
+                )
+                update_count += 1
+            else:
+                # The record does not exist, insert it
+                app_table.put_item(Item=record)
+                insert_count += 1
+
+        # Return the counts as a JSON object
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': '*',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+            },
+            'body': {
+                'insert_count': insert_count,
+                'update_count': update_count
             }
-
-            # Insert each row from the 2nd row onward to DynamoDB
-            # Commented out the following code
-            # for index, row in df.iloc[1:].iterrows():
-            #     item = {column: str(value) for column, value in row.iteritems()}
-            #     app_table.put_item(Item=item)
+        }
 
 
     # GET /{id} アクセスの場合はこちら
